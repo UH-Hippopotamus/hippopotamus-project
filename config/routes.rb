045b1aa65308fceeead6cae52648da472e950199
Chris <cgyeager@hawaii.edu>
2018-03-16 20:21:20 -1000

    alert, alert type, alert msg pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
   root 'pages#home'
+	
+	get 'pages/alert_type'
+
+	get 'pages/alert'
+
+	get 'pages/alert_message'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
