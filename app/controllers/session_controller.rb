4893fd005e1f2f05de1970fbaa65972ee40f2674
Chris <cgyeager@hawaii.edu>
2018-04-06 14:45:59 -1000

    can select alert type, display current user in header

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -1,11 +1,15 @@
 class SessionController < ApplicationController
+
   def new
   end
 
 	def create
+		$current_user = ""
+
 		account = Account.find_by(username: params[:session][:username])
 		if account && account.authenticate(params[:session][:password])
 			# log in
+			$current_user = account.username
 			redirect_to alert_type_path
 		else
 			redirect_to login_path
