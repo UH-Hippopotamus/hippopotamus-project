8e007fa7ca5140a38d82f338d11522a2ce905651
Chris <cgyeager@hawaii.edu>
2018-04-26 01:06:07 -1000

    added validations to alerts, and testing for alerts

--- app/models/alert.rb
+++ app/models/alert.rb
@@ -1,2 +1,9 @@
 class Alert < ApplicationRecord
+	validates :alert_type, presence: true
+	validates :emergency_type, presence: true
+	validates :affected_areas, presence: true
+	validates :alert_message, presence: true
+	validates :user, presence: true
+	validates :status, presence: true
 end
+
--- test/models/alert_test.rb
+++ test/models/alert_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class AlertTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "should create valid alert" do
+		@alert = Alert.new(alert_type: "alert", emergency_type: "emergency", affected_areas: "area", alert_message: "message", user: "user", status: "status", created_at: Time.now.to_datetime)
+		assert @alert.valid?
+	end
+
+  test "should create invalid alert missing field 0" do
+		@alert = Alert.new(emergency_type: "emergency", affected_areas: "area", alert_message: "message", user: "user", status: "status", created_at: Time.now.to_datetime)
+    assert_not @alert.valid?
+  end
+
+  test "should create invalid alert missing field 1" do
+		@alert = Alert.new(alert_type: "alert", affected_areas: "area", alert_message: "message", user: "user", status: "status", created_at: Time.now.to_datetime)
+    assert_not @alert.valid?
+  end
+
+  test "should create invalid alert missing field 2" do
+		@alert = Alert.new(alert_type: "alert", emergency_type: "emergency", alert_message: "message", user: "user", status: "status", created_at: Time.now.to_datetime)
+    assert_not @alert.valid?
+  end
+
+  test "should create invalid alert missing field 3" do
+		@alert = Alert.new(alert_type: "alert", emergency_type: "emergency", affected_areas: "area", user: "user", status: "status", created_at: Time.now.to_datetime)
+    assert_not @alert.valid?
+  end
+
+  test "should create invalid alert missing field 4" do
+		@alert = Alert.new(alert_type: "alert", emergency_type: "emergency", affected_areas: "area", alert_message: "message", status: "status", created_at: Time.now.to_datetime)
+    assert_not @alert.valid?
+  end
+
+  test "should create invalid alert missing field 5" do
+		@alert = Alert.new(alert_type: "alert", emergency_type: "emergency", affected_areas: "area", alert_message: "message", user: "user", created_at: Time.now.to_datetime)
+		assert_not @alert.valid?
+	end
+
+
 end
+
